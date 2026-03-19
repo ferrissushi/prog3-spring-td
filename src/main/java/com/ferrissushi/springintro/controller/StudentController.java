0c0fcba80ebed1efef3c495a1e8f7f517ab95093
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-19 18:19:15 +0300

    fix: replace controller annotation to rest controller

--- src/main/java/com/ferrissushi/springintro/controller/StudentController.java
+++ src/main/java/com/ferrissushi/springintro/controller/StudentController.java
@@ -2,18 +2,18 @@ package com.ferrissushi.springintro.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ferrissushi.springintro.entity.Student;
 import com.ferrissushi.springintro.service.StudentService;
 
 import lombok.AllArgsConstructor;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class StudentController {
 
