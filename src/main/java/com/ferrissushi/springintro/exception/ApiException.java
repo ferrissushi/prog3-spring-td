37c2d13222b17dc900a790c22a2c0c3aae5e1b7f
NickyHariniaina <nickyharini1na@gmail.com>
2026-03-25 13:48:38 +0300

    feat(api): add ApiException

---
+++ src/main/java/com/ferrissushi/springintro/exception/ApiException.java
@@ -0,0 +1,18 @@
+package com.ferrissushi.springintro.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiException extends Exception {
+    private String message;
+    private HttpStatus httpStatus;
+
+    public ApiException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+}
